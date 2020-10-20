e71b5713cd43de0dde942356ea9866de54830cf6
Warren Dixon <warrenwdixon@sbcglobal.net>
2020-10-20 10:14:10 -0500

    Oct20 added MovieRepository

---
+++ app/src/main/java/com/example/android/recyclerview/MovieRepository.java
@@ -0,0 +1,48 @@
+package com.example.android.recyclerview;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.util.List;
+
+public class MovieRepository {
+    private MovieDAO mMovieDAO;
+    private LiveData<List<Movie>> mAllMovies;
+
+    //constructor that gets a handle to the database and initializes the member variables
+    MovieRepository(Application application) {
+        AppDatabase db = AppDatabase.getInstance(application);
+        mMovieDAO = db.mMovieDAO();
+        mAllMovies = mMovieDAO.getAllMovies();
+    }
+
+    // wrapper method called getAllMovies() that returns the cached words as LiveData
+    // Room executes on a separate thread. Observed LiveData notifies the observer when data changes
+    LiveData<List<Movie>> getAllMovies() { return mAllMovies;}
+
+    // wrapper for insert method, must use AsyncTask to call insert() on non-UI thread or app will crash
+    // Room ensures no long-running operations on the main thread which would block UI
+    public void insert (Movie movie) {
+        Log.d("WWD", "in repository insert call insertAsyncTask");
+        new insertAsyncTask(mMovieDAO).execute(movie);
+    }
+
+    // create insertAsyncTask as an inner class
+    private static class insertAsyncTask extends AsyncTask<Movie, Void, Void> {
+        private MovieDAO mAsyncTaskDao;
+
+        insertAsyncTask(MovieDAO dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Movie... params) {
+            Log.d("WWD","in MovieRepository insertAsyncTask doInBackground");
+            mAsyncTaskDao.nukeTable();
+            //mAsyncTaskDao.insertMovie(params[0]);
+            return null;
+        }
+    }
+}
