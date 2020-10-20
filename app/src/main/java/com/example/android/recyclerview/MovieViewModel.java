7216ba4923efa3871c1c7c30be16c05bcbe070c4
Warren Dixon <warrenwdixon@sbcglobal.net>
2020-10-20 10:31:14 -0500

    Oct 20 added MovieViewModel

---
+++ app/src/main/java/com/example/android/recyclerview/MovieViewModel.java
@@ -0,0 +1,38 @@
+package com.example.android.recyclerview;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModelProvider;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.List;
+
+public class MovieViewModel extends ViewModelProvider.AndroidViewModelFactory {
+
+    private MovieRepository mRepository;
+
+    // private LiveData member variable to cache list of words
+    private LiveData<List<Movie>> mAllMovies;
+
+    /**
+     * Creates a {@code AndroidViewModelFactory}
+     *
+     * @param application an application to pass in {@link AndroidViewModel}
+     */
+    public MovieViewModel(@NonNull Application application) {
+        super(application);
+        mRepository = new MovieRepository(application);
+        mAllMovies = mRepository.getAllMovies();
+    }
+
+    // add a "getter" method that gets all the words, hides implementation from UI
+    LiveData<List<Movie>> getAllMovies() { return mAllMovies;}
+
+    // create wrapper insert() method that calls Repository's insert() method,
+    // implementation of insert() is hidden from UI
+    public void insert(Movie movie) {
+        Log.d("WWD", "in MovieViewModel insert, call repository insert");
+        mRepository.insert(movie);
+    }
+}
